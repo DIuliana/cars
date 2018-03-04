a2f2897ad59b9ba8bfb0e631c9beb0114518a59b
DIuliana <danea.iuliana@gmail.com>
2018-03-04 11:57:36 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+This is a starter project using Spring Boot. On master branch, the config is made so the app can pe deployed on a server. Dev branch has usual Spring Boot App config(embeded Tomcat).
---
+++ src/main/java/hello/HelloController.java
@@ -0,0 +1,17 @@
+package hello;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class HelloController {
+
+    @GetMapping
+    public String hello() {
+
+        return "Hello!";
+    }
+}
