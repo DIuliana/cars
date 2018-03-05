cc1b041e57d758706c87e8ffde56a19ca56e4f49
DIuliana <danea.iuliana@gmail.com>
2018-03-05 20:49:26 +0200

    Added a static html page and some scipts in order to connect to Facebook.

--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -1,12 +1,11 @@
 package hello;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/")
+@RequestMapping("/hello")
 public class HelloController {
 
     @GetMapping
